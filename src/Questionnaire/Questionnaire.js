f9beaf9b063ae231227e9595411a18d5dd773311
Matthew Kirschnick <matthew.kirschnick@node.energy>
2022-01-06 15:12:28 +0100

    Add basic display of questions

--- src/Questionnaire/Questionnaire.js
+++ src/Questionnaire/Questionnaire.js
@@ -1,13 +1,22 @@
-import React from "react";
-
 import { Controls } from "./Controls/Controls";
 import { Questions } from "./Questions/Questions";
 import { Results } from "./Results/Results";
 
 function Questionnaire() {
+  const questions = [
+    {
+      label: "this is a question?",
+      name: "question1",
+      choices: [
+        { value: "opt1", label: "option 1" },
+        { value: "opt2", label: "option 2" },
+      ],
+    },
+  ];
+
   return (
     <div className="Questionnaire">
-      <Questions />
+      <Questions questions={questions} />
       <Results />
       <Controls />
     </div>
--- src/Questionnaire/Questions/Questions.js
+++ src/Questionnaire/Questions/Questions.js
@@ -1,7 +1,52 @@
-import React from "react";
+import { useState } from "react";
+import { FormGroup, Input, Label } from "reactstrap";
 
-function Questions() {
-  return <div className="Questions">questions</div>;
+function Questions({ questions }) {
+  const [values, setValues] = useState([]);
+
+  function handleChange(index, value) {
+    const newValues = [...values];
+
+    newValues[index] = value;
+    setValues(newValues);
+  }
+
+  return (
+    <div className="Questions">
+      {questions.map((question, index) => (
+        <Question
+          key={question.name}
+          label={question.label}
+          name={question.name}
+          choices={question.choices}
+          selectedChoice={values[index]}
+          onChange={(value) => handleChange(index, value)}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Question({ label, name, choices, selectedChoice, onChange }) {
+  return (
+    <FormGroup tag="fieldset">
+      <legend>{label}</legend>
+      {choices.map((choice) => (
+        <FormGroup check key={choice.label}>
+          <Input
+            id={`${name}-${choice.value}`}
+            name={name}
+            type="radio"
+            checked={choice.value === selectedChoice}
+            onChange={() => onChange(choice.value)}
+          />{" "}
+          <Label for={`${name}-${choice.value}`} check>
+            {choice.label}
+          </Label>
+        </FormGroup>
+      ))}
+    </FormGroup>
+  );
 }
 
 export { Questions };
