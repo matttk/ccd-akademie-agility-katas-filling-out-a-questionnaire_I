4bc9b6512866429f76cd2f2797d3890cd8208dad
Matthew Kirschnick <matthew.kirschnick@node.energy>
2022-01-06 19:17:00 +0100

    Show right/wrong and, if needed, correction

--- src/Questionnaire/Results/Answers.tsx
+++ src/Questionnaire/Results/Answers.tsx
@@ -1,4 +1,5 @@
-import { Question } from "../Questions/Questions";
+import { Question, QuestionChoice } from "../Questions/Questions";
+import { isCorrect } from "./utils";
 
 interface AnswersProps {
   questions: Array<Question>;
@@ -6,7 +7,55 @@ interface AnswersProps {
 }
 
 function Answers(props: AnswersProps): JSX.Element {
-  return <div className="Answers">answers</div>;
+  const { questions, answers } = props;
+
+  return (
+    <div className="Answers">
+      {questions.map((question, index) => (
+        <AnswerInfo
+          key={question.label}
+          question={question}
+          answer={answers[index]}
+        />
+      ))}
+    </div>
+  );
+}
+
+interface AnswerInfoProps {
+  question: Question;
+  answer: string | undefined;
+}
+
+function AnswerInfo(props: AnswerInfoProps): JSX.Element {
+  const { question, answer } = props;
+  const isAnswerCorrect: boolean = isCorrect(question, answer);
+  const correctChoice: QuestionChoice | undefined = question.choices.find(
+    (choice) => choice.correct
+  );
+
+  function getCorrectnessText(
+    answer: string | undefined,
+    isCorrect: boolean
+  ): string {
+    if (isCorrect) {
+      return `Your answer '${answer}' is correct`;
+    }
+
+    return `Your answer '${answer}' is wrong`;
+  }
+
+  return (
+    <div className="AnswerInfo">
+      <p>{question.label}</p>
+      <ul>
+        <li>{getCorrectnessText(answer, isAnswerCorrect)}</li>
+        {!isAnswerCorrect && !!correctChoice && (
+          <li>Correct answer: '{correctChoice.label}'</li>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export { Answers };
